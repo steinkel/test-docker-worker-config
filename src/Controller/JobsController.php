f7e8b6c5bf7512aaa67ac71c0e38f99a92b528e7
Jorge González <steinkel@gmail.com>
2025-08-06 23:26:58 +0100

    add controller and job

---
+++ src/Controller/JobsController.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Job\ExampleJob;
+use Cake\Queue\Mailer\QueueTrait;
+use Cake\Queue\QueueManager;
+
+/**
+ * Jobs Controller
+ *
+ */
+class JobsController extends AppController
+{
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        QueueManager::push(ExampleJob::class, ['some' => 'data']);
+        return $this->response->withStringBody('Job added to queue');
+    }
+
+}
---
+++ src/Job/ExampleJob.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Job;
+
+use Cake\Log\Log;
+use Cake\Queue\Job\JobInterface;
+use Cake\Queue\Job\Message;
+use Interop\Queue\Processor;
+
+/**
+ * Example job
+ */
+class ExampleJob implements JobInterface
+{
+    /**
+     * Executes logic for ExampleJob
+     *
+     * @param \Cake\Queue\Job\Message $message job message
+     * @return string|null
+     */
+    public function execute(Message $message): ?string
+    {
+        Log::info('ExampleJob executed');
+        return Processor::ACK;
+    }
+}
